daee31777a833cb6d1d37723ff17a1be448453c6
anderson.vidal <anderson.vidal@virtus.ufcg.edu.br>
2018-12-03 17:14:50 -0300

    initialization springboot

---
+++ src/main/java/com/cccpharmaapi/cccpharmaapi/CccPharmaApiApplication.java
@@ -0,0 +1,12 @@
+package com.cccpharmaapi.cccpharmaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CccPharmaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CccPharmaApiApplication.class, args);
+    }
+}
