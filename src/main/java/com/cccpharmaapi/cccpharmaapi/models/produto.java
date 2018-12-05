faecd5be58ebd75049ded11d9b8d3749e73c9ea5
anderson.vidal <anderson.vidal@virtus.ufcg.edu.br>
2018-12-05 16:39:08 -0300

    entidade produto

---
+++ src/main/java/com/cccpharmaapi/cccpharmaapi/models/produto.java
@@ -0,0 +1,74 @@
+package com.cccpharmaapi.cccpharmaapi.models;
+
+import javax.persistence.*;
+
+@Entity
+public class produto {
+
+    @Column
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private int id;
+
+    @Column
+    private String nome;
+
+    @Column
+    private String fabricante;
+
+    @Column
+    private String categoria;
+
+    @Column
+    private Double preco;
+
+    @Column
+    private int quantidade;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+}
