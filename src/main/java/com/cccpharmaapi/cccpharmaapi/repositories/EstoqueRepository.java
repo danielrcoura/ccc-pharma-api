ec6eb506c78a6d50b119734939784e302bb383f2
AndersonVidal <anderson.vidal@ccc.ufcg.edu.br>
2018-12-07 00:13:55 -0300

    repositories

--- src/main/java/com/cccpharmaapi/cccpharmaapi/repositories/EstoqueRepository.java
+++ src/main/java/com/cccpharmaapi/cccpharmaapi/repositories/EstoqueRepository.java
@@ -1,10 +1,10 @@
 package com.cccpharmaapi.cccpharmaapi.repositories;
 
-import com.cccpharmaapi.cccpharmaapi.models.Produto;
+import com.cccpharmaapi.cccpharmaapi.models.Estoque;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+public interface EstoqueRepository extends JpaRepository<Estoque, Integer>{
 
 }
--- src/main/java/com/cccpharmaapi/cccpharmaapi/repositories/LoteRepository.java
+++ src/main/java/com/cccpharmaapi/cccpharmaapi/repositories/LoteRepository.java
@@ -1,10 +1,10 @@
 package com.cccpharmaapi.cccpharmaapi.repositories;
 
-import com.cccpharmaapi.cccpharmaapi.models.Produto;
+import com.cccpharmaapi.cccpharmaapi.models.Lote;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+public interface LoteRepository extends JpaRepository<Lote, Integer> {
 
 }
