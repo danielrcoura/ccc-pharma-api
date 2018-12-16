a52997a2e1f58c97e587e1b4c29ce30a58e4b033
AndersonVidal <anderson.vidal@ccc.ufcg.edu.br>
2018-12-16 16:54:40 -0300

    configure cors 6

--- src/main/java/com/cccpharmaapi/cccpharmaapi/security/WebSecurityConfig.java
+++ src/main/java/com/cccpharmaapi/cccpharmaapi/security/WebSecurityConfig.java
@@ -25,8 +25,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.cors();
-
         http.csrf().disable().authorizeRequests()
                 .antMatchers("/").permitAll()
                 .antMatchers(HttpMethod.GET, "/estoques/*").permitAll()
