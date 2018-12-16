0db71858c6b7bf0eadd2cd964f268d21cee41b55
AndersonVidal <anderson.vidal@ccc.ufcg.edu.br>
2018-12-16 16:37:37 -0300

    configure cors 3

--- src/main/java/com/cccpharmaapi/cccpharmaapi/security/PharmaCORSFilter.java
+++ src/main/java/com/cccpharmaapi/cccpharmaapi/security/PharmaCORSFilter.java
@@ -17,7 +17,7 @@ public class PharmaCORSFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
 
-        response.setHeader("Access-Control-Allow-Origin", request.getHeader("*"));
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
