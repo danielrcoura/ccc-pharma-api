1e210a019959fb1a69783d3308519239654429c9
AndersonVidal <anderson.vidal@ccc.ufcg.edu.br>
2018-12-16 16:29:49 -0300

    configure cors 2

--- src/main/java/com/cccpharmaapi/cccpharmaapi/security/WebSecurityConfig.java
+++ src/main/java/com/cccpharmaapi/cccpharmaapi/security/WebSecurityConfig.java
@@ -12,8 +12,8 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
